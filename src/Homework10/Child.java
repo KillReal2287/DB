4135db60d8e2a006a46b7d3b70c44f6d019e9941
KillReal2287 <89987040+KillReal2287@users.noreply.github.com>
2022-10-10 11:44:09 +0700

    deleted unnecessary comment.

--- src/Homework10/Child.java
+++ src/Homework10/Child.java
@@ -3,23 +3,6 @@ package Homework10;
 
 public class Child implements Runnable{
     @Override
-    /*public void run() {
-        for(int i = 0; i<10; i++) {
-            System.out.println("Hello from child");
-            synchronized (this){
-                this.notify();
-                Parent.queue = false;
-                if(i != 9)
-                    while (!Parent.queue) {
-                        try {
-                            this.wait();
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-            }
-        }
-    }*/
     public void run() {
         for(int i = 0; i<10; i++) {
             System.out.println("Hello from child ");
