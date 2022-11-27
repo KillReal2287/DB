becac46b3388e1de6511d29c92a29d6890daf1c4
KillReal2287 <89987040+KillReal2287@users.noreply.github.com>
2022-11-27 15:54:42 +0700

    remade 12th

--- src/Homework12/Child.java
+++ src/Homework12/Child.java
@@ -1,15 +1,13 @@
 package Homework12;
 
-import java.util.LinkedList;
-import java.util.List;
-
 public class Child implements Runnable {
-    volatile static LinkedList<String> list = new LinkedList<>();
-    private final Parent parent;
-    Child(Parent parent){
-        this.parent = parent;
+    List list;
+    
+    public Child(List list) {
+        this.list = list;
     }
 
+    @Override
     public void run() {
         while (true){
             try{
@@ -17,21 +15,7 @@ public class Child implements Runnable {
             } catch (InterruptedException e){
                 System.out.println(e);
             }
-            bubbleSort(list);
-        }
-    }
-
-    private void  bubbleSort(List<String> list){
-        synchronized (parent) {
-            for (int i = 0; i < list.size()-1; i++) {
-                for (int j = 0; j < list.size()-1; j++) {
-                    if ( list.get(j).compareToIgnoreCase(list.get(j+1)) > 0 ){
-                        String tmp = list.get(j);
-                        list.set(j, list.get(j+1));
-                        list.set(j+1, tmp);
-                    }
-                }
-            }
+            list.sorter();
         }
     }
 }
