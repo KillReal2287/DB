666ba1bcccd3f7dcd2666a4b78d1cd4ad3882aa4
KillReal2287 <89987040+KillReal2287@users.noreply.github.com>
2022-11-27 15:54:37 +0700

    remade 12th

--- src/Homework12/Parent.java
+++ src/Homework12/Parent.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Parent {
     public static void main(String[] args) {
-        Parent parent = new Parent();
         Scanner scanner = new Scanner(System.in);
-        String str;
-        Thread thread = new Thread(new Child(parent));
-        thread.start();
-        while(true){
+        List list = new List();
+        String str = "";
+        Thread child = new Thread(new Child(list));
+        child.start();
+        while (true){
             str = scanner.nextLine();
-            if(str.equals("")){
-                System.out.println(Child.list);
+            if (str.equals("")){
+                list.display();
             }
-            else {
-                    Child.list.addFirst(str);
+            else{
+                list.insertFirst(str);
             }
         }
     }
